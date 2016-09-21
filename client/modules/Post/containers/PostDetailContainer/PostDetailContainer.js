f178cc0aa2cc0d8628e4ab72bd7696edb9bd85e6
Aleh Savonevich <savonevich.dem@gmail.com>
2016-09-21 11:41:59 +0300

    ui for detail page was changed

--- client/modules/Post/containers/PostDetailContainer/PostDetailContainer.js
+++ client/modules/Post/containers/PostDetailContainer/PostDetailContainer.js
@@ -2,8 +2,12 @@ import React, { PropTypes, Component } from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import Helmet from 'react-helmet';
+import Paper from 'material-ui/Paper';
+import Avatar from 'material-ui/Avatar';
+import ListItem from 'material-ui/List/ListItem';
+var dateFormat = require('dateformat');
 
-import styles from '../../components/PostListItem/PostListItem.css';
+import styles from './PostDetailContainer.css';
 import { fetchPost } from '../../PostActions';
 
 class PostDetailPage extends Component {
@@ -18,11 +22,23 @@ class PostDetailPage extends Component {
         <div>
           <Helmet title={this.props.post.title}/>
           <div className={`${styles['single-post']} ${styles['post-detail']}`}>
-            <h3 className={styles['post-title']}>{this.props.post.title}</h3>
-            <p className={styles['author-name']}>
-              By @savonevich
+            <div>
+              <ListItem
+                disabled={true}
+                leftAvatar={
+                  <Avatar src={ require('./../../../../../server/images/users/sav.jpg') } size="45" />
+                }
+                primaryText={ <p className={ styles['primary-text']} >by savonevich</p> }
+                secondaryText={ dateFormat(this.props.post.dateAdded, "mmmm dS, yyyy, h:MM TT") }
+              >
+              </ListItem>
+            </div>
+            <p>
+              <img className={ styles['post-image'] } src={ require('./../../../../../server/images/posts/' + this.props.post.cover_url) } />
             </p>
-            <p className={styles['post-desc']}>{this.props.post.content}</p>
+            <h3 className={styles['post-title']}>{ this.props.post.title }</h3>
+            <p className={styles['post-preview']}><b>{ this.props.post.preview }</b></p>
+            <p className={styles['post-desc']}>{ this.props.post.content }</p>
           </div>
         </div>
       );
